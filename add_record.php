bc0f24796d045b9d84b84bf58a092fa5cda01296
Phuriwat <phuriwat.wor@hotmail.com>
2018-03-12 15:11:20 +0000

    Add record page

--- add_record.php
+++ add_record.php
@@ -39,9 +39,86 @@ session_start();
     </div>
   </nav>
 
-  <div class="container">
-    <h3>Add new record</h3>
-  </div>
+  
+<div class="container">
+  <h2>Add record</h2>
+  <form class="form-horizontal" action="/action_page.php">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="location">Location:</label>
+      <div class="col-sm-3">
+        <select class="form-control" id="location" placeholder="Select location" name="site">
+          <option value="Site 1">Site 1</option>
+          <option value="Site 2">Site 2</option>
+          <option value="Site 3">Site 3</option>
+        </select>
+        
+      </div>
+    </div>
+    
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Date:</label>
+      <div class="col-sm-3">          
+            <input class="form-control" type="date" name="date">
+      </div>
+      
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="type">Type:</label>
+      <div class="col-sm-3">
+        <select class="form-control" id="type" name="type">
+          <option value="Metal">Metal</option>
+          <option value="Wood">Wood</option>
+          <option value="Rock">Rock</option>
+        </select>
+        
+      </div>
+    </div>
+	 <div class="form-group">
+      <label class="control-label col-sm-2" for="context">Context:</label>
+      <div class="col-sm-3">
+        <select class="form-control" id="context" name="context">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        
+      </div>
+    </div>
+	<div class="form-group">
+      <label class="control-label col-sm-2" for="trench">Area:</label>
+      <div class="col-sm-3">
+        <select class="form-control" id="trench" name="trench">
+          <option value="1">trench 1</option>
+          <option value="2">trench 2</option>
+          <option value="3">trench 3</option>
+          <option value="4">trench 4</option>
+          <option value="5">trench 5</option>
+        </select>
+        
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="description">Description:</label>
+      <div class="col-sm-10">    
+      <textarea class="form-control" rows="5" id="description"></textarea>
+       </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+    
+  </form>
+</div>
 
 </body>
 </html>
