55713c4da5ff3058128560e1754e5ce46485d29e
cheetah <inchaiya.keta@gmail.com>
2018-03-28 00:57:21 +0400

    improved navigation bar and reformatted code

--- search.php
+++ search.php
@@ -14,13 +14,14 @@ session_start();
     <!-- jQuery UI -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <!-- Font Awesome -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
             integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ"
             crossorigin="anonymous"></script>
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -28,7 +29,7 @@ session_start();
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php">Dig Data</a>
+            <a class="navbar-brand" href="index.php"><i class="fas fa-paint-brush"></i>&nbsp;DigData</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
@@ -37,22 +38,21 @@ session_start();
                 <li class="active"><a href="search.php">Search</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <?php
-                    $_SESSION["currentPage"] = basename($_SERVER['PHP_SELF']);
+                <li><?php
                     include 'PHP/LoginButton.php';
                     ?>
-                </li>
             </ul>
         </div>
     </div>
 </nav>
 
-<div class="container">
+<div class="container" style="margin-top:50px">
+    <h3>Search</h3>
     <div class="row">
         <div class="col-sm-4">
             <div class="panel panel-default">
-                <div class="panel-heading"><h4 class="text-center">Search Filter&nbsp;<i class="fas fa-filter"></i></h4></div>
+                <div class="panel-heading"><h4 class="text-center">Search Filter&nbsp;<i class="fas fa-filter"></i></h4>
+                </div>
                 <div class="panel-body">
                     <form class="form-horizontal" action="#">
                         <div class="form-group">
--- view_record.php
+++ view_record.php
@@ -1,102 +1,107 @@
-<!DOCTYPE html>
 <?php
 session_start();
-$findID=$_GET["id"];
+$findID = $_GET["id"];
 
-$username="is5108group-4";
-$password="b9iVc.9gS8c7NJ";
-$host="is5108group-4.host.cs.st-andrews.ac.uk";
-$db="is5108group-4__digdata";
+$username = "is5108group-4";
+$password = "b9iVc.9gS8c7NJ";
+$host = "is5108group-4.host.cs.st-andrews.ac.uk";
+$db = "is5108group-4__digdata";
 $tb = "Finds";
 
-$connect= mysqli_connect($host,$username,$password);
-	if (!$connect )
-		{
-			echo "Can't connect to SQLdatabase ";
-			exit();
-		}
-	else
-	{
-		mysqli_select_db($connect,$db) or die("Can't select Database");
-		//SELECT *,Finds.Description AS 'FDESC',cr.Description AS 'CRDESC' FROM Finds INNER JOIN Context_Records cr ON Finds.ContextID = cr.ContextID INNER JOIN Site s ON s.SiteCode = cr.SiteCode ORDER BY `Date` DESC
-    $find = mysqli_query($connect,"SELECT *,Finds.Description AS 'FDESC',cr.Description AS 'CRDESC' FROM $tb INNER JOIN Context_Records cr
+$connect = mysqli_connect($host, $username, $password);
+if (!$connect) {
+    echo "Can't connect to SQLdatabase ";
+    exit();
+} else {
+    mysqli_select_db($connect, $db) or die("Can't select Database");
+    //SELECT *,Finds.Description AS 'FDESC',cr.Description AS 'CRDESC' FROM Finds INNER JOIN Context_Records cr ON Finds.ContextID = cr.ContextID INNER JOIN Site s ON s.SiteCode = cr.SiteCode ORDER BY `Date` DESC
+    $find = mysqli_query($connect, "SELECT *,Finds.Description AS 'FDESC',cr.Description AS 'CRDESC' FROM $tb INNER JOIN Context_Records cr
       ON Finds.ContextID = cr.ContextID INNER JOIN Site s ON s.SiteCode = cr.SiteCode WHERE $tb.FindID = $findID");
     $row = mysqli_fetch_array($find, MYSQLI_ASSOC);
-		//echo "SELECT * FROM $tb WHERE Username='$LOGusername' AND Password='$LOGpassword'";
-		//echo $found;
-	}
+    //echo "SELECT * FROM $tb WHERE Username='$LOGusername' AND Password='$LOGpassword'";
+    //echo $found;
+}
 ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>DigData</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/styles.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>DigData</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"
+            integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ"
+            crossorigin="anonymous"></script>
 </head>
 <body>
-	<nav class="navbar navbar-inverse">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php">Dig Data</a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Home</a></li>
-          <li><a href="add_record.php">Add Record</a></li>
-          <li><a href="search.php">Search</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><?php
-		  include 'PHP/LoginButton.php';
-			?>
-        </ul>
-      </div>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><i class="fas fa-paint-brush"></i>&nbsp;DigData</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="add_record.php">Add Record</a></li>
+                <li><a href="search.php">Search</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><?php
+                    include 'PHP/LoginButton.php';
+                    ?>
+            </ul>
+        </div>
     </div>
-  </nav>
-<div class="container">
-<div class="row">
- <div class="col-sm-12">
-         <table class="table table-hover table-bordered">
-             <tbody>
-             <tr>
-                 <td width="100">
-                     <img src="https://png.icons8.com/metro/1600/batman-new.png" height="100" width="100"
-                          class="center-block" alt="Cinque Terre">
-                 </td>
-                 <td>
-                     <table class="table table-bordered">
-                         <tbody>
-                         <tr>
-                             <td><strong>ID: </strong><?php printf ("%s", $row["FindID"])?></td>
-                         </tr>
-                         <tr>
-                             <td>
-                                 <strong>Location: </strong><?php printf ("%s", $row["SiteName"])?> <strong>Founder: </strong><?php printf ("%s", $row["UserID"])?> <strong>Date: </strong><?php printf ("%s", $row["Date"])?>
-                             </td>
-                         </tr>
-                         <tr>
-                             <td><strong>Description: </strong><?php printf ("%s", $row["FDESC"])?>
-                             </td>
-                         </tr>
-                         <tr>
-                             <td>Lorem ipsum donec id elit non mi porta gravida at eget metus.</td>
-                         </tr>
-                         </tbody>
-                     </table>
-                 </td>
-             </tr>
-             </tbody>
-         </table>
-     </div>
- </div>
+</nav>
+
+<div class="container" style="margin-top:50px">
+    <div class="row">
+        <div class="col-sm-12">
+            <table class="table table-bordered">
+                <tbody>
+                <tr>
+                    <td width="100">
+                        <img src="https://png.icons8.com/metro/1600/batman-new.png" height="100" width="100"
+                             class="center-block" alt="Cinque Terre">
+                    </td>
+                    <td>
+                        <table class="table table-bordered">
+                            <tbody>
+                            <tr>
+                                <td><strong>ID: </strong><?php printf("%s", $row["FindID"]) ?></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Location: </strong><?php printf("%s", $row["SiteName"]) ?>
+                                    <strong>Founder: </strong><?php printf("%s", $row["UserID"]) ?>
+                                    <strong>Date: </strong><?php printf("%s", $row["Date"]) ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><strong>Description: </strong><?php printf("%s", $row["FDESC"]) ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Lorem ipsum donec id elit non mi porta gravida at eget metus.</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 </body>
 </html>
